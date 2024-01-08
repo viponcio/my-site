518d8a310c7e39e614347c00df60e5764be75fe1
viponcio <vitoria.poncio@gmail.com>
2024-01-08 17:48:05 -0300

    icones corretos

--- src/Bio/About.tsx
+++ src/Bio/About.tsx
@@ -1,12 +1,18 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 
-function About(IconProp) {
-    FontAwesomeIcon.icon: IconProp
+
+function About() {
     return (
     <div className="flex justify-center">
         <h1 className="text-lg absolute top-64">Olá! Muito prazer, sou a Vitória Poncio! Desenvolvedora front end.</h1>
-        <FontAwesomeIcon icon="fa-brands fa-linkedin-in" />
+        <div className="flex flex-row space-x-4 absolute top-72">
+          <a href="https://www.linkedin.com/in/viponcio/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedinIn} size="3x"/></a>
+          <a href={'mailto:vitoria.poncio@gmail.com'} rel='noopener noreferrer'><FontAwesomeIcon icon={faEnvelope} size="3x"/></a>
+          <a href="https://github.com/viponcio" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} size="3x"/></a>
+        </div>
     </div>
-  );
+  );    
 }
 export default About;
