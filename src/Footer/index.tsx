b753c78b23762fdd518ca69f600df80710ac8660
viponcio <vitoria.poncio@gmail.com>
2024-01-29 11:12:37 -0300

    fix

--- src/Footer/index.tsx
+++ src/Footer/index.tsx
@@ -2,7 +2,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from '@fortawesome/free-regular-svg-icons'
 function Footer() {
     return (
-      <div className="text-center w-full md:mt-96">
+      <div className="text-center w-full  md:mt-96">
           <h1 className="text-gray-600">Desenvolvido com <FontAwesomeIcon icon={faHeart} size="1x"/> por <a href="https://github.com/viponcio" target="_blank" rel="noopener noreferrer">Vitória Poncio</a> </h1>
       </div>
   );    
